b18ce23c8d63d5b6dc327dd45f31df1266e2a607
Holly Henske <hcover333@gmail.com>
2021-07-05 07:46:21 -0500

    hours later. Same problems.

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -13,9 +13,18 @@ export class Satellite {
         this.operational = operational;
 
     }
-
+    //this function gets the error "not a function" so ....
     shouldShowWarning() : boolean {
-        return this.type === 'Space Debris'
+        //return this.type === 'Space Debris'
+        //the above was given in a help session 
+        if (this.type.toLowerCase() === 'space debris') {
+            //something here needs to tell it use warning class
+            return true;
+            //text: "Instead of setting [class.ol-style] equal to a string, 
+            //the changeColor variable is a boolean defined in the skill-set.component.ts file.
+            // If changeColor is true, Angular adds the ol-style class of the tag. If changeColor 
+            //is false, the class remains absent from the tag."
+        }
     }
             
     
